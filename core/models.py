53f4b6ceb195e6037ac0ce7e38eb118d3c57357e
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-25 21:53:39 +0200

    Add model fields

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
@@ -5,9 +6,24 @@ class User(AbstractUser):
     pass
 
 
-class Employees:
-    pass
+class Employees(models.Model):
+    first_name = models.CharField(null=True, max_length=50)
+    last_name = models.CharField(null=True, max_length=50)
+    email = models.EmailField(null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
 
 
-class Shifts:
-    pass
+class Shifts(models.Model):
+    SHIFT_TIMES = [
+        ('0-8', '0-8'),
+        ('8-16', '8-16'),
+        ('16-24', '16-24')
+    ]
+    shift_date = models.DateField()
+    shift_time = models.CharField(choices=SHIFT_TIMES)
+    employee = models.ForeignKey(Employees, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.shift_date
