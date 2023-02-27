e7c76498765ea6ec31fd316e5cbd15da30f2060a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-27 21:20:45 +0200

    Create test for shifts creation.

--- core/tests.py
+++ core/tests.py
@@ -20,3 +20,27 @@ class EmployeesTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Employees.objects.count(), 1)
         self.assertEqual(Employees.objects.get().first_name, 'John')
+
+
+class ShiftsTests(APITestCase):
+
+    def setUp(self):
+        self.employee = Employees.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="john@doe.com",
+        )
+
+    def test_create_shifts(self):
+        """
+        Ensure we can create a new shifts object.
+        """
+        url = reverse('shifts-list')
+        data = {
+            "shift_date": "2023-02-23",
+            "shift_time": "0-8",
+            "employee": 1
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Shifts.objects.count(), 1)
